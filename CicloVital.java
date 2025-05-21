3074a882549a33d1739d13e931eece951bb1ae0e
Pau-Munoz <paumunale2@alu.edu.gva.es>
2025-05-21 14:27:28 +0200

    Añadidas todas las clases del proyecto sin GIT.

---
+++ CicloVital.java
@@ -0,0 +1,7 @@
+public interface CicloVital {
+    static final int NATALIDAD_MAXIMA = 1;
+    static final int VITALIDAD_MAXIMA = 2;
+
+    public void reproducir();
+    public void envejecer();
+}
---
+++ Ciudadano.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Ciudadano {
+    protected String nombre;
+    protected int poblacion;
+    private int CONTADOR = 1;
+
+    public Ciudadano() {
+        this.nombre = "Ciudadano" + CONTADOR;
+        this.poblacion = CONTADOR;
+        CONTADOR ++;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public void censar(ArrayList<Ciudadano>ciudadanos){
+
+    }
+
+    public void poblacionesTotales(ArrayList<Ciudadano>ciudadanos){
+
+    }
+
+    public void morir(ArrayList<Ciudadano>ciudadanos){
+
+    }
+}
